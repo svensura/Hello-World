7636d5d12b1e98d6cdd9b7aee708759957e19ff3
ssura <svensura@germanscreens.de>
2019-09-06 16:54:12 +0200

    Initial Commit

---
+++ Hello World/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Hello World
+//
+//  Created by Sven on 06.09.19.
+//  Copyright © 2019 Sven. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
